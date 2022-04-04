e60f33d69362ceaeea615ae13d5b04e3b74f8f14
tn4799 <tn4799@gmail.com>
2022-04-04 15:57:19 +0200

    moved files into src-folder

--- src/RegisterRollerMissionVehicles.lua
+++ src/RegisterRollerMissionVehicles.lua
@@ -1,5 +1,5 @@
 RegisterRollerMissionVehicles = {
-    filename = g_currentModDirectory .. "missionVehicles.xml"
+    filename = g_currentModDirectory .. "xml/missionVehicles.xml"
 }
 
 g_missionManager:loadMissionVehicles(RegisterRollerMissionVehicles.filename)
