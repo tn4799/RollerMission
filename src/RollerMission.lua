1935bc8ec8c95dd29bac776c8aa93fad17a76d83
tn47 <tn4799@gmail.com>
2022-04-06 20:28:50 +0200

    fixed references and added mission method

--- src/RollerMission.lua
+++ src/RollerMission.lua
@@ -7,13 +7,14 @@ local RollerMission_mt = Class(RollerMission, AbstractFieldMission)
 InitObjectClass(RollerMission, "RollerMission")
 
 function RollerMission.new(isServer, isClient, customMt)
-	local self = AbstractFieldMission.new(isServer, isClient, customMt or PlowMission_mt)
+	local self = AbstractFieldMission.new(isServer, isClient, customMt or RollerMission_mt)
+
 	self.workAreaTypes = {
 		[WorkAreaType.ROLLER] = true
 	}
 	self.rewardPerHa = RollerMission.REWARD_PER_HA
 	self.reimbursementPerHa = 0
-	local groundTypeMapId, groundTypeFirstChannel, groundTypeNumChannels = self.mission.fieldGroundSystem:getDensityMapData(FieldDensityMap.GROUND_TYPE)
+	local groundTypeMapId, groundTypeFirstChannel, groundTypeNumChannels = self.mission.fieldGroundSystem:getDensityMapData(FieldDensityMap.ROLLER_LEVEL)
 	self.completionModifier = DensityMapModifier.new(groundTypeMapId, groundTypeFirstChannel, groundTypeNumChannels, self.mission.terrainRootNode)
 	self.completionFilter = DensityMapFilter.new(self.completionModifier)
 	local rollerValue = self.mission.fieldGroundSystem:getFieldGroundValue(FieldGroundType.ROLLED_SEEDBED)
@@ -54,7 +55,6 @@ function RollerMission.canRunOnField(field, sprayFactor, fieldSpraySet, fieldPlo
 end
 
 function RollerMission:getData()
-	print("getData")
 	return {
 		location = string.format(g_i18n:getText("fieldJob_number"), self.field.fieldId),
 		jobType = g_i18n:getText("FieldJob_jobType_rolling"),
@@ -63,7 +63,13 @@ function RollerMission:getData()
 	}
 end
 
+function RollerMission:partitionCompletion(x, z, widthX, widthZ, heightX, heightZ)
+	self.completionModifier:setParallelogramWorldCoords(x, z, widthX, widthZ, heightX, heightZ, DensityCoordType.POINT_VECTOR_VECTOR)
 
+	local _, area, totalArea = self.completionModifier:executeGet(self.completionFilter)
+
+	return area, totalArea
+end
 
 function RollerMission:validate(event)
     return event == FieldManager.FIELDEVENT_SOWN or event == FieldManager.FIELDEVENT_GROWING
