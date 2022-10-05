8c6edf7b448c5ed982be71cc3f5669ed3838601d
tn4799 <tn4799@gmail.com>
2022-10-05 13:29:07 +0200

    fixed mission type assign and generation

--- src/RollerMissionGenerator.lua
+++ src/RollerMissionGenerator.lua
@@ -1,6 +1,9 @@
 RollerMissionGenerator = {}
 
 function RollerMissionGenerator:generateMissions(dt)
+    self.missionTypes = Utils.shuffle(self.missionTypes)
+    --[[ 
+    -- alternative version to generate roller missions for sure by not depending on the order of mission types.
     local numActionsLeft = MissionManager.MAX_TRIES_PER_GENERATION
 	local numMissionsLeft = MissionManager.MAX_MISSIONS_PER_GENERATION
 	local indices = {}
@@ -14,22 +17,22 @@ function RollerMissionGenerator:generateMissions(dt)
 	for _, index in ipairs(indices) do
         local field = g_fieldManager.fields[index]
 
-        if field.fieldMissionAllowed then
+        if field.fieldMissionAllowed and not RollerMissionGenerator.hasFieldRollerMission(field.fieldId) then
             local fieldSpraySet, sprayFactor, fieldPlowFactor, limeFactor, weedFactor, maxWeedState, stubbleFactor, rollerFactor = self:getFieldData(field)
             local canRun, fieldState, growthState, weedState, args = RollerMission.canRunOnField(field, sprayFactor, fieldSpraySet, fieldPlowFactor, limeFactor, maxWeedState, stubbleFactor, rollerFactor)
 
             if canRun then
                 local mission = RollerMission.new(true, g_client ~= nil)
-                mission.type = "roll"
+                mission.type = g_missionManager:getMissionType("roll")
 
                 if mission:init(field, sprayFactor, fieldSpraySet, fieldPlowFactor, fieldState, growthState, limeFactor, weedFactor, weedState, stubbleFactor, rollerFactor, args) then
                     self:assignGenerationTime(mission)
                     mission:register()
                     table.insert(self.missions, mission)
+                    g_messageCenter:publish(MessageType.MISSION_GENERATED)
 
                     numMissionsLeft = numMissionsLeft - 1
-
-                    if numMissionsLeft <= 0 then 
+                    if numMissionsLeft <= 0 then
                         break
                     end
                 else
@@ -44,17 +47,26 @@ function RollerMissionGenerator:generateMissions(dt)
             break
         end
     end
+    ]]
 end
 
-MissionManager.generateMissions = Utils.prependedFunction(MissionManager.generateMissions, RollerMissionGenerator.generateMissions)
+function RollerMissionGenerator.hasFieldRollerMission(fieldId)
+    for _, mission in pairs(g_missionManager.missions) do
+        if mission.field.fieldId == fieldId then
+            return true
+        end
+    end
+    return false
+end
 
 function RollerMissionGenerator:startMission(startedMission, farmId, spawnVehicles)
     local fieldId = startedMission.field.fieldId
     for _, mission in pairs(self.missions) do
-        if mission.field.fieldId == fieldId then
+        if mission.field.fieldId == fieldId and mission ~= startedMission then
             mission:delete()
         end
     end
 end
 
+MissionManager.generateMissions = Utils.prependedFunction(MissionManager.generateMissions, RollerMissionGenerator.generateMissions)
 MissionManager.startMission = Utils.appendedFunction(MissionManager.startMission, RollerMissionGenerator.startMission)
